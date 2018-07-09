ee4bbee5842ec7344ca240f90210f121879c656c
VilleKemp <kempo.ville@gmail.com>
2018-07-09 15:52:10 +0300

    moved environment scripts to jenkinstesting

--- environment/start/start_browsermob.sh
+++
@@ -1,4 +0,0 @@
-#start browsermob at port 8181
-cd /home/ville/browsermob-proxy-2.1.4/bin/
-./browsermob-proxy -port 8181 
-
--- environment/start/start_etracker.sh
+++
@@ -1,2 +0,0 @@
-#start etracker
-python /home/ville/synopsys/ExerciseTracker/forum.py
